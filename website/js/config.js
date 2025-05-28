79e27ac4755618535a5a2c20d66dd7e199a348f3
hydror <remy.roche75@gmail.com>
2025-05-28 08:54:13 +0200

    Delete website directory

--- website/js/config.js
+++
@@ -1,9 +0,0 @@
-// website/js/config.js
-// Configuration for the frontend application
-
-// API base URL for backend communication
-const API_BASE_URL = 'http://127.0.0.1:5001/api';
-
-// Exporting the configuration to be used in other modules if needed (using ES6 modules as an example)
-// If not using ES6 modules in the browser directly, these would be global or accessed differently.
-// For this refactoring, we'll assume these can be accessed by other scripts if loaded in the correct order.
